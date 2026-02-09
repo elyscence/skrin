edda5f2de1b79b3caccb18a0978c25681613df2a
nextmorning <demidovme@mail.ru>
2026-02-09 20:39:53 +0300

    replaced whole ugly structure and main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-mod telegram;
 mod web;
+
+use axum::{
+    Router,
+    routing::{get, post},
+};
+
+use tracing::debug;
+
+use crate::web::handlers;
 
 #[tokio::main]
 async fn main() {
-    web::routes::run_axum_server().await;
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let app = Router::new()
+        .route("/health", get(handlers::health))
+        .route("/upload_form", get(handlers::show_form))
+        .route("/file/{file_name}", get(handlers::get_file))
+        .route("/upload", post(handlers::upload));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    debug!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
