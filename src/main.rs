4a27ecd3bb750a0efb072cebca63d28027570741
nextmorning <demidovme@mail.ru>
2026-02-09 19:00:51 +0300

    added get_file route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use axum::{
     Router,
-    extract::Multipart,
-    http::StatusCode,
+    body::Body,
+    extract::{Multipart, Path},
+    http::{StatusCode, header},
     response::{Html, IntoResponse},
     routing::{get, post},
 };
+use tokio_util::io::ReaderStream;
 use tracing::debug;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+const UPLOAD_PATH: &str = "./upload";
 
 #[tokio::main]
 async fn main() {
@@ -22,6 +26,7 @@ async fn main() {
     let app = Router::new()
         .route("/health", get(health))
         .route("/upload_form", get(show_form))
+        .route("/file/{file_name}", get(get_file))
         .route("/upload", post(upload));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -40,7 +45,7 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
 
         debug!("Length of `{}`, name: {}", name, file_name);
 
-        let formatted_path = format!("./upload/{}", file_name);
+        let formatted_path = format!("{}/{}", UPLOAD_PATH, file_name);
 
         if let Ok(true) = tokio::fs::try_exists(&formatted_path).await {
             return (StatusCode::BAD_REQUEST, "Upload failed");
@@ -54,6 +59,49 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
     }
 
     (StatusCode::CREATED, "Image uploaded")
+}
+
+async fn get_file(
+    Path(file_name): Path<String>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let upload_dir = std::path::PathBuf::from(UPLOAD_PATH)
+        .canonicalize()
+        .map_err(|_| {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Upload dir error".to_string(),
+            )
+        })?;
+
+    let formatted_path = upload_dir.join(&file_name);
+
+    if !formatted_path.starts_with(&upload_dir) {
+        return Err((StatusCode::BAD_REQUEST, "Invalid file path".to_string()));
+    }
+
+    let file = tokio::fs::File::open(&formatted_path)
+        .await
+        .map_err(|err| (StatusCode::NOT_FOUND, format!("File not found: {}", err)))?;
+
+    let content_type = match mime_guess::from_path(&formatted_path).first_raw() {
+        Some(mime) => mime,
+        None => {
+            return Err((
+                StatusCode::BAD_REQUEST,
+                "MIME Type couldn't be determined".to_string(),
+            ));
+        }
+    };
+
+    let stream = ReaderStream::new(file);
+    let body = Body::from_stream(stream);
+
+    let headers = [
+        (header::CONTENT_TYPE, content_type),
+        (header::CONTENT_DISPOSITION, "inline"),
+    ];
+
+    Ok((headers, body))
 }
 
 async fn show_form() -> Html<&'static str> {
